759dc97b495dc2a75fff20c8b8e310ca22eb98b0
bomin <yk051016@gmail.com>
2024-11-15 11:20:04 +0900

    테스트

---
+++ Servlet_Test/ExampleServlet2.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class ExampleServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+	
+	public void init(ServletConfig config) throws ServletException {
+		 System.out.println("init 호출");
+	}
+
+
+	public void destroy() {
+		 System.out.println("destroy 호출");
+	}
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 System.out.println("service 호출");
+	
+	}
+	}
+
