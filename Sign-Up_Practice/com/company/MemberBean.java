1ad645718a528a7c9d5c6d119b11023fbe3a257c
bomin <yk051016@gmail.com>
2024-11-15 11:12:52 +0900

    회원가입창 연습

---
+++ Sign-Up_Practice/com/company/MemberBean.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberBean {
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	private String id;
+	private String pwd;
+	private String name;
+	private String birth;
+	private String email;
+	
+}
+
