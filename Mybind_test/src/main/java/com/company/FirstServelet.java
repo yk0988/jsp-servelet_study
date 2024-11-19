60bbbaf190140851f235d3d5122eefb46f028a70
bomin <yk051016@gmail.com>
2024-11-19 21:39:46 +0900

    포워딩+ 바인딩 연습

---
+++ Mybind_test/src/main/java/com/company/FirstServelet.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/first_bind")
+public class FirstServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		request.setAttribute("address","대한민국 경기도");
+		response.sendRedirect("second_bind");
+	}
+}
---
+++ Mybind_test/src/main/java/com/company/ex02/FirstServelet.java
@@ -0,0 +1,25 @@
+package com.company.ex02;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/first_bind2")
+public class FirstServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		request.setAttribute("address","대한민국 경기도");
+		RequestDispatcher dispatch =request.getRequestDispatcher("second_bind2");
+		dispatch.forward(request, response);
+	}
+}
